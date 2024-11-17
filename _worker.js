201f9eed6bfd751a3ca79e76c7fbcaf2865f5744
weaponchiang <332310153@qq.com>
2024-11-17 21:11:17 +0800

    更新API

--- _worker.js
+++ _worker.js
@@ -89,7 +89,7 @@ export default {
  * @returns {Promise<void>}
  */
 async function fetchAndDecryptData() {
-  const apiUrl = 'https://application.xyhk.us.kg/serverlist';
+  const apiUrl = 'http://api.skrapp.net/api/serverlist';
   const headers = { 
     'accept': '/', 
     'appversion': '1.3.1', 
